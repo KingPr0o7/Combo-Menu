89fbf3359efd9b1896445fb2ce585566dd2c8d1b
Nathan Parker <natecoleparker@gmail.com>
2023-02-22 18:44:00 -0500

    Type Cases and Start on Embeds

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
+// Settings 
+const typing_speed = 50;
+
+// Immutables
+const sandwich_typecases = ['chicken', 'beef', 'tofu', 'c', 'b', 't'];
+const size_typecases = ['small', 'medium', 'large', 's', 'm', 'l'];
+var bot_typing_status = false;
+var chat_count = 1;
+
+// Elements
 const cursor = document.getElementById("cursor");
+const form = document.getElementById('forum-content');
+const old_element = document.getElementById('human-chat');
+const submit_forum = document.getElementById('submit-input');
+const submit_button = document.getElementById('submit');
+const container_holder = document.getElementById('chat-container');
+const container = document.getElementById('chat-content');
+const sandwich_options = document.getElementById('sandwich-options');
+old_element.remove();
+
+// Functions
 function input(msg) {
 	let bot_msg = document.getElementById('bot-msg');
 	bot_msg.textContent = '';
 	bot_msg.appendChild(cursor);
-	type("bot-msg", msg, 50, 0, true);
+	document.getElementById('bot-msg').appendChild(sandwich_options);
+	type("bot-msg", msg, typing_speed, 0, true);
 }
-
-const submit_forum = document.getElementById('submit-input');
-const submit_button = document.getElementById('submit');
-
-submit_button.addEventListener('click', function(event) {
-	event.preventDefault();
-	add_chat(submit_forum.value)
-	submit_forum.value = ''
-});
 
 function add_chat(msg) {
-	var container_holder = document.getElementById('chat-container')
-	var container = document.getElementById('chat-content')
-	var old_element = document.getElementById('human-chat')
-	var clone = old_element.cloneNode(true)
-	clone.children[0].innerHTML = msg
+	chat_count++;
+	var clone = old_element.cloneNode(true);
+	clone.children[0].innerHTML = msg;
+	clone.classList.remove('starter-human-chat');
 	container.appendChild(clone);
-	container_holder.scrollBy(0, 2000)
-	document.getElementById('bot-msg').removeChild(cursor)
+	container_holder.scrollBy(0, 2000);
+	if (chat_count % 2 == 0) {
+		document.getElementById('bot-msg').removeChild(cursor);
+	}
 }
 
-// "Real" Typing Characters
 function type(targetElement, textToType, speed, index, cursorMode) {
 	let element = document.getElementById(`${targetElement}`);
 	let text = `${textToType}`;
 	if (index < text.length) {
-		cursor.removeAttribute('blinking', '')
+		cursor.removeAttribute('blinking', '');
+		bot_typing_status = true;
 		if (cursorMode == true) {
 			element.removeChild(cursor);
 			element.innerHTML += text.charAt(index);  
@@ -44,5 +57,41 @@ function type(targetElement, textToType, speed, index, cursorMode) {
 		setTimeout(type, speed, `${targetElement}`, text, speed, index, cursorMode);
 	} else {
 		cursor.setAttribute('blinking', '');
+		bot_typing_status = false;
 	}
 }
+
+function input_border(toggle) {
+	if (toggle == true) {
+		submit_forum.style.boxShadow = 'inset 0 0 0 2px red';
+		submit_forum.style.color = 'red';
+	} else {
+		submit_forum.style.boxShadow = 'none';
+		submit_forum.style.color = 'white';
+	}
+}
+
+// Events 
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	if (bot_typing_status == false) {
+		if (!sandwich_typecases.includes(submit_forum.value.toLowerCase())) {
+			input_border(true);
+		} else {
+			input_border(false);
+			add_chat(submit_forum.value);
+			submit_forum.value = '';
+		}
+	}
+});
+
+
+submit_forum.addEventListener('input', function() {
+	if (sandwich_typecases.includes(submit_forum.value.toLowerCase())) {
+		input_border(false);
+	}
+	if (submit_forum.value == '') {
+		input_border(false);
+	}
+});
